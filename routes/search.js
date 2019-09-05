c4c08d4b10cab3632d5e87fb60c37de6f9095be5
Vivian Lo <yunyubee@gmail.com>
2019-09-05 18:10:07 +0800

    Add sort feature on index page

--- routes/search.js
+++ routes/search.js
@@ -3,14 +3,21 @@ const router = express.Router()
 const RestaurantList = require('../models/restaurantList')
 
 router.get('/', (req, res) => {
-  RestaurantList.find((err, restaurantLists) => {
-    if (err) return console.log(err)
-    const restaurant = restaurantLists.filter(restaurant => {
-      const regex = new RegExp(req.query.keyword, 'gi')
-      return restaurant.name_en.match(regex) || restaurant.name.match(regex) || restaurant.category.match(regex)
+  const sortKey = req.query.sortKey || 'rating';
+  const sortOrder = req.query.sortOrder || 'desc';
+  const sortObject = {};
+  sortObject[sortKey] = sortOrder;
+
+  RestaurantList.find()
+    .sort(sortObject)
+    .exec((err, restaurantLists) => {
+      if (err) return console.log(err)
+      const restaurant = restaurantLists.filter(restaurant => {
+        const regex = new RegExp(req.query.keyword, 'gi')
+        return restaurant.name_en.match(regex) || restaurant.name.match(regex) || restaurant.category.match(regex)
+      })
+      return res.render('index', { restaurants: restaurant, keyword: req.query.keyword })
     })
-    return res.render('index', { restaurants: restaurant, keyword: req.query.keyword })
-  })
 })
 
 module.exports = router
