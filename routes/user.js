75776aff83faee43153e6f291e9bb1b513884edd
Vivian Lo <yunyubee@gmail.com>
2019-09-10 14:37:50 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,18 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+  User.findOne({ email: email })
+    .then(user => {
+      if (user) {
+        res.render('register', { name, email, password, password2 })
+      } else {
+        const newUser = new User({ name, email, password })
+        newUser.save()
+          .then(user => { res.redirect('/') })
+          .catch(err => console.log(err))
+      }
+    })
 })
 
 router.get('/logout', (req, res) => {
