ac04fbfdf85ec8d2be8bfb6ddbb4f3f65f905f62
Vivian Lo <yunyubee@gmail.com>
2019-09-10 14:42:45 +0800

    Install express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const port = 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 
 //connect to database
@@ -17,6 +18,13 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected')
 })
+
+//express-session
+app.use(session({
+  secret: "your secret key", // secret: 定義一組屬於你的字串做為私鑰
+  resave: false,
+  saveUninitialized: true,
+}))
 
 const RestaurantList = require('./models/restaurantList')
 
