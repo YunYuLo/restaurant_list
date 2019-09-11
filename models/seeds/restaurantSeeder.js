550c34310de8f0b7d2060f7d1c2d7ec07a3e09b7
Vivian Lo <yunyubee@gmail.com>
2019-09-11 22:18:31 +0800

    create seeder of restaurant and sample user

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+//Models
 const RestaurantList = require('../restaurantList')
-const restaurantJson = require('./restaurant.json')
+const User = require('../user')
+//Seed data
+const restaurantJson = require('./restaurant.json').results
+const userJson = require('./user.json').results
 
-mongoose.connect('mongodb://127.0.0.1/restaurantList', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1/restaurantList', { useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
@@ -13,7 +18,29 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('db connected')
 
-  RestaurantList.create(restaurantJson.results)
+
+  for (let i = 0; i < userJson.length; i++) {
+    //create user
+    const user = User(userJson[i])
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        user.save()
+          .then()
+          .catch(err => { console.log(err) })
+      })
+    })
+
+    //map 1 user(index: i) with 3 restaurants(index: j)
+    for (let j = i * 3; j < (i + 1) * 3; j++) {
+      RestaurantList.create({
+        ...restaurantJson[j],
+        userId: user._id,
+      })
+
+      if (j === restaurantJson.length) return
+    }
+  }
 
   console.log('done!')
 })
